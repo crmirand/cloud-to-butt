a472c78cc92c7f9408b36057620270bc47edc4f3
crmirand <crmirand@ucsd.edu>
2015-08-16 12:10:27 -0700

    Updated content_script.js
    Changed cloud to butt to change different words

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bMatty\b/g, "Matty the bitch");
+	v = v.replace(/\bmatty\b/g, "matty the bitch");
+	v = v.replace(/\bMathew\b/g, "Matty the bitch");
+	v = v.replace(/\bmathew\b/g, "matty the bitch");
 	
 	textNode.nodeValue = v;
 }

033fff2b1735e9a6f6fb7d5a962bb405245335e4
Chris Miranda <crmirand@ucsd.edu>
2015-08-16 16:04:34 -0700

    Working version

---
+++ Source/content_script.js
@@ -0,0 +1,44 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bMatty\b/g, "Matty the bitch");
+	v = v.replace(/\bmatty\b/g, "matty the bitch");
+	v = v.replace(/\bMathew\b/g, "Matty the bitch");
+	v = v.replace(/\bmathew\b/g, "matty the bitch");
+	v = v.replace(/\bMatthew\b/g, "Matty the bitch");
+	v = v.replace(/\bmatthew\b/g, "matty the bitch");
+	
+	textNode.nodeValue = v;
+}
+
+
